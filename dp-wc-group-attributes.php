c7d04e45f759d787446ba3e679a76de204b80f57
Dirk Peeters <ict-dirk.peeters@telenet.be>
2016-01-05 15:38:39 +0100

    Update dp-wc-group-attributes.php

--- dp-wc-group-attributes.php
+++ dp-wc-group-attributes.php
@@ -1,1 +1,74 @@
+<?php
+/*
+* Plugin Name: dp wc group attributes
+* Plugin URI: http://www.dp-ict.be/
+* Description: dp-wc-group-attributes.
+* Version:     0.1-alpha
+* Author:      Dirk Peeters
+* Author URI:  ict-dirk.peeters@telenet.be
+* License: GPL2
+* License URI: http://www.gnu.org/licenses/gpl-2.0.html
+* Text Domain: dp-wc-group-attributes
+*/
 
+/*
+* Prefix guide
+* - wp: wordpress
+* - wc: woocommerce
+* - dp: first initials of the author or company
+* - lb: first initials of the plugin name
+* -- 16: option(year of build)
+*
+* example: wpdp_
+*/
+
+/* Hint:
+* Code your project well
+* Learn to comment what your doing
+* by adding comments you make your code better to understand by others or even yourself when re-developing it.
+*/
+
+/* Table of contents */
+
+/*
+    0. Security
+      0.1 - protect it from being accessed directly
+      0.2 - check if woocommerce is active
+    1. Hooks
+    2. SchortCodes
+    3. Filters
+    4. External Scripts
+    5. Actions
+    6. Helpers
+    7. Custom Post Types
+    8. Admin pages
+    9. Settings
+    */
+
+/* 0. Security */
+  // 0.1 - protect it from being accessed directly
+      if ( ! defined( 'ABSPATH' ) ) { 
+          exit; // Exit if accessed directly
+        }
+  
+  // 0.2 - check if woocommerce is active
+      if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+          $wcActive = true;
+        }
+/* 1. Hooks */
+
+/* 2. SchortCodes */
+
+/* 3. Filters */
+
+/* 4. External Scripts */
+
+/* 5. Actions */
+
+/* 6. Helpers */
+
+/* 7. Custom Post Types */
+
+/* 8. Admin pages */
+
+/* 9. Settings */
