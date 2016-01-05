103700a7722a27025a8a755f3c7df4de2657a485
Dirk Peeters <ict-dirk.peeters@telenet.be>
2016-01-05 19:30:55 +0100

    Update dp-wc-group-attributes.php

--- dp-wc-group-attributes.php
+++ dp-wc-group-attributes.php
@@ -3,7 +3,7 @@
 * Plugin Name: dp wc group attributes
 * Plugin URI: http://www.dp-ict.be/
 * Description: dp-wc-group-attributes.
-* Version:     0.1-alpha
+* Version:     0.0.1
 * Author:      Dirk Peeters
 * Author URI:  ict-dirk.peeters@telenet.be
 * License: GPL2
@@ -16,7 +16,7 @@
 * - wp: wordpress
 * - wc: woocommerce
 * - dp: first initials of the author or company
-* - lb: first initials of the plugin name
+* - ga: first initials of the plugin name
 * -- 16: option(year of build)
 *
 * example: wpdp_
@@ -75,15 +75,78 @@
 /* 9. Settings */
 
 /* test stuff */
+
 if ( $wcActive == true ) // our previous wc check
 { 
+    // hint: registers all our custom shortcodes on init
+    add_action('init', 'dpwcga_register_shortcodes');
+
+    
+    function dpwcga_register_shortcodes() {
+	
+	add_shortcode('dpga_form', 'dpwcga_form_shortcode');
+	
+    }
+    
+    function dpwcga_form_shortcode( $args, $content="")
+    {
+        // get the group id
+        $dpwcga_ID = 0;
+        if( isset($args['id']) ) $dpwcga_ID = (int)$args['id'];
+        
+        // setup our output variable - the form html 
+	$output = '
+	
+		<div class="dpga">
+		
+			<form id="dpga_form" name="dpga_form" class="dpga-form" method="post"
+			
+			
+				<input type="hidden" name="dpga_list" value="'. $dpwcga_ID .'">
+			
+				<p class="dpga-input-container">
+				
+					<label>Group Name</label><br/>
+					<input type="text" name="dpga_gname" placeholder="Group Name" /><br/>
+				
+				</p>
+				<p class="dpga-input-container">
+				
+					<label>Group Slug</label><br/>
+					<input type="text" name="dpga_gslug" placeholder="Group Slug" /><br/>
+				
+				</p>';
+				
+				// including content in our form html if content is passed into the function
+				if( strlen($content) ):
+				
+					$output .= '<div class="dpga-content">'. wpautop($content) .'</div>';
+				
+				endif;
+				
+				// completing our form html
+				$output .= '<p class="dpga-input-container">
+				
+					<input type="submit" name="dpga_submit" value="Add Group" />
+				
+				</p>
+			
+			</form>
+		
+		</div>
+	
+	';
+	
+	// return our results/html
+	return $output;
+    }
     // if true run the code
-    function dpwcga_myFunction()
+    function dpwcga_create_aGroup()
     {
         //do stuff
     }
     // create the attribute group
-    function dpwcga_create_aGroup()
+    function dpwcga_remove_aGroup()
     {
         //do stuff
     }
